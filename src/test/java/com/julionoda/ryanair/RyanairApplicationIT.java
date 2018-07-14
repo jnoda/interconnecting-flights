f8ce476a88c5db4963a54fdba90020c7ed9ad9d3
jnoda <julio.noda@gmail.com>
2018-07-14 11:25:44 +0200

    Improved support for integration tests

---
+++ src/test/java/com/julionoda/ryanair/RyanairApplicationIT.java
@@ -0,0 +1,25 @@
+package com.julionoda.ryanair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Integration test suite for {@link RyanairApplication}.
+ * 
+ * @author jnoda
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class RyanairApplicationIT {
+
+	/**
+	 * Tests that the Spring Boot context loads correctly.
+	 */
+	@Test
+	public void contextLoads() {
+	}
+}
